65defc167a8dc6bcfce5d165e6f5142d8e7fa771
Anderson Vasconcelos <ciclista-rs@hotmail.com>
2021-05-11 18:21:51 -0300

    Finish

---
+++ kharacters.py
@@ -0,0 +1,48 @@
+from csv import DictReader, DictWriter
+from os import close
+import os.path
+
+
+def find_all_characters(filename):
+    list = []
+    with open(filename, "r") as f:
+        for row in DictReader(f):
+            list.append(row)
+
+        if not list:
+            return []
+        else:
+            return list
+
+
+def find_character_by_id(filename, character_id):
+    with open(filename, "r+") as f:
+        open_file = f.readlines()
+        f.close()
+        try:
+            return dict(zip(open_file[0].split(','), open_file[character_id].split(',')))
+        except:
+            return None
+        
+def create_character(filename, **kwargs):
+    headers = ['id', 'name', 'intelligence', 'power', 'strength', 'agility']
+    if not os.path.isfile(filename):
+        with open(filename, "a+") as f:
+            writer = DictWriter(f, fieldnames=headers)
+            writer.writeheader()
+            f.close()
+
+    with open(filename, "r+") as f:
+        open_file = f.readlines()
+        last_id = open_file[-1].split(',')[0]
+        if last_id == 'id':
+            last_id = 1
+        else:
+            last_id = int(last_id) + 1
+        kwargs = [{**kwargs, 'id': last_id}]
+        writer = DictWriter(f, fieldnames=headers)
+        writer.writerows(kwargs)
+        f.close()
+    return kwargs
+    
+
